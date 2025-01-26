b08a25560c1419b153ad5d0cfc6e7ff09d2ac948
yama-kei <yamazaki@mierune.co.jp>
2025-01-26 18:28:19 +0900

    ラスタータイルとベクトルタイルをsvelte-maplibre-glで表示

--- 04-01-static-directory/my-map/src/routes/+page.svelte
+++ 04-01-static-directory/my-map/src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import {
+		MapLibre,
+		NavigationControl,
+		ScaleControl,
+		GlobeControl,
+		RasterTileSource,
+		VectorTileSource,
+		RasterLayer,
+		CircleLayer
+	} from 'svelte-maplibre-gl';
+	import { page } from '$app/stores';
+
+	const raster_url = $page.url.href + 'raster/{z}/{x}/{y}.png';
+	const vector_url = $page.url.href + 'vector/{z}/{x}/{y}.pbf';
+	console.log('vector_url:', vector_url);
+</script>
+
+<MapLibre
+	class="h-[100vh] min-h-[300px]"
+	style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+	zoom={3.5}
+	center={{ lng: 137, lat: 36 }}
+>
+	<RasterTileSource tiles={[raster_url]} minzoom={0} maxzoom={6} tileSize={256}>
+		<RasterLayer />
+	</RasterTileSource>
+
+	<VectorTileSource tiles={[vector_url]} minzoom={0} attribution="国土数値情報 - 学校データ">
+		<CircleLayer sourceLayer="vector" paint={{ 'circle-radius': 5, 'circle-color': 'red' }} />
+	</VectorTileSource>
+
+	<!-- Add Control  -->
+	<NavigationControl />
+	<ScaleControl />
+	<GlobeControl />
+</MapLibre>
