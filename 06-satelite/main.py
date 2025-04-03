86a5cce4dfa8a299b1e66916f93129609a5be30e
yama-kei <yamazaki@mierune.co.jp>
2025-04-04 06:51:30 +0900

    rio-tilerをインポートし、/rgbnir.pngエンドポイントを追加

--- 06-satelite/main.py
+++ 06-satelite/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from rio_tiler.io import Reader
 
 app = FastAPI()
 
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/rgbnir.png")
+async def make_image():
+    with Reader()
