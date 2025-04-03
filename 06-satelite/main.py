44c0f80eb97ca9779de2354f96fcd825f6d3af4a
yama-kei <yamazaki@mierune.co.jp>
2025-04-04 08:56:30 +0900

    Dockerfileにシステム依存関係を追加し、main.pyのピラミッド画像から低解像度のデータを読み込むエンドポイントを追加

--- 06-satelite/main.py
+++ 06-satelite/main.py
@@ -12,15 +12,15 @@ def health():
 
 @app.get("/rgbnir.png")
 async def make_image():
-    with Reader("static/rgbnir.tif") as image:
+    with Reader("static/rgbnir.tif", options={"unscale": 0.1}) as image:
         imgdata = image.read([1, 2, 3])
     png = imgdata.render(img_format="PNG", **img_profiles.get("png"))
     return Response(png, media_type="image/png")
 
 
-@app.get("/rgbnir_cog/png")
+@app.get("/rgbnir_cog.png")
 async def make_image_cog():
     with Reader("static/rgbnir_cog.tif") as image:
-        imgdata = image.preview([1, 2, 3])
-    png = imgdata.render(img_format="PNG", **img_profiles.get("png"))
+        imgdata = image.preview([1, 2, 3])  # band-1, 2, 3
+        png = imgdata.render(img_format="PNG", **img_profiles.get("png"))
     return Response(png, media_type="image/png")
