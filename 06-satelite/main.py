bab26168be4c1c28677ee0c90dce9cad2e5b8a74
yama-kei <yamazaki@mierune.co.jp>
2025-04-04 18:37:23 +0900

    /rgbnir_remote_cog_part.pngエンドポイントを追加し、指定された範囲のリモートCOG画像をスケーリングして返す機能を実装

--- 06-satelite/main.py
+++ 06-satelite/main.py
@@ -43,3 +43,27 @@ async def make_image_remote_cog(scale_min: float, scale_max: float):
         imgdata.rescale(((scale_min, scale_max),))
         png = imgdata.render(img_format="PNG", **img_profiles.get("png"))
     return Response(png, media_type="image/png")
+
+
+@app.get("/rgbnir_remote_cog_part.png")
+async def make_image_remote_cog_part(
+    minx: float,
+    miny: float,
+    maxx: float,
+    maxy: float,
+    max_size: int = 256,
+    scale_min: float = 0,
+    scale_max: float = 2000,
+):
+    with Reader(
+        "http://fileserver/rgbnir_cog.tif",
+    ) as image:
+        imgdata = image.part(
+            bbox=(minx, miny, maxx, maxy),
+            indexes=(1, 2, 3),
+            dst_crs="EPSG:32654",
+            max_size=max_size,
+        )
+        imgdata.rescale(((scale_min, scale_max),))
+        png = imgdata.render(img_format="PNG", **img_profiles.get("png"))
+    return Response(png, media_type="image/png")
