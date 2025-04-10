b84f1f525324bd853f0b3d483a4d56a5144124c9
yama-kei <yamazaki@mierune.co.jp>
2025-04-10 21:52:15 +0900

    Dockerfileとmain.pyを更新し、システム依存関係を追加。衛星画像検索機能を実装し、指定地点の衛星画像を取得するエンドポイントを追加。

--- 07-advanced/api/app/main.py
+++ 07-advanced/api/app/main.py
@@ -1,7 +1,9 @@
+import httpx
 import psycopg2
 import psycopg2.pool
-from fastapi import Depends, FastAPI, HTTPException, Response
+from fastapi import Depends, FastAPI, HTTPException, Response, status
 from fastapi.middleware.cors import CORSMiddleware
+from rio_tiler.io import Reader
 
 from app.model import PoiCreate, PoiUpdate
 
@@ -167,3 +169,69 @@ def update_point(id: int, data: PoiUpdate, conn=Depends(get_connection)):
             "id": id,
         },
     }
+
+
+async def search_dataset(
+    minx: float, miny: float, maxx: float, maxy: float, limit: int = 12
+):
+    """STACを使って衛星画像を検索"""
+
+    url = "http://earth-search.aws.element84.com/v1/collections/sentinel-2-l2a/items"
+    params = {
+        "limit": limit,
+        "bbox": f"{minx},{miny},{maxx},{maxy}",
+    }
+    headers = {
+        "Accept": "application/json",
+    }
+
+    async with httpx.AsyncClient() as client:
+        res = await client.get(url, params=params, headers=headers)
+        res.raise_for_status()
+        dataset = res.json()
+
+        return dataset
+
+
+@app.get("/points/{point_id}/satelite.jpg")
+async def satelite_preview(
+    point_id: int, max_size: int = 256, conn=Depends(get_connection)
+):
+    """DBに登録された地点を指定して、その地点を含む衛生画像を検索"""
+    if max_size > 1024:
+        return Response(status_code=status.HTTP_400_BAD_REQUEST)
+
+    # pointsテーブルから指定したIDのデータを取得
+    with conn.cursor() as cur:
+        cur.execute(
+            "SELECT ST_X(geom) as longitude, ST_Y(geom) as latitude FROM points WHERE id = %s",
+            (point_id,),
+        )
+        res = cur.fetchone()
+
+    # データが存在しない場合は404エラーを返す
+    if not res:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+    longtitude, latitude = res
+
+    # 衛生画像を検索
+    # 便宜上、地点の周囲0.01度の範囲で検索
+    buffer = 0.01
+    minx = longtitude - buffer
+    miny = latitude - buffer
+    maxx = longtitude + buffer
+    maxy = latitude + buffer
+
+    result = await search_dataset(minx, miny, maxx, maxy, limit=1)
+    if len(result["features"]) == 0:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+    feature = result["features"][0]
+    cog_url = feature["assets"]["visual"]["href"]
+
+    with Reader(cog_url) as src:
+        img = src.preview(max_size=max_size)
+    jpg = img.render(img_format="JPEG")
+
+    return Response(content=jpg, media_type="image/jpg")
